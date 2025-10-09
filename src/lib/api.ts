ed264026a8c9e7a008ea0e874cd8d1e1221d90ef
João Alves <joaovitoralves157@gmail.com>
2025-10-09 14:31:47 -0300

    .

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:3110";
-/*export const API_BASE_URL = "https://dimensiona.genustecnologia.com.br/api";*/
+/*export const API_BASE_URL = "http://localhost:3110";*/
+export const API_BASE_URL = "https://dimensiona.genustecnologia.com.br/api";
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
